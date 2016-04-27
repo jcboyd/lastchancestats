39c559de26e3df6363931dd2c6d43c2ec3142c8f
jcboyd <boyd.joseph.c@gmail.com>
2016-04-27 13:44:19 +0200

    first commit

--- Demos/knnDemo.m
+++ Demos/knnDemo.m
@@ -1,45 +1,38 @@
 clear all;
-rng default;
+load('knn_data.mat');
 
-% Set model parameters
-k = 7;
+% set model parameters
+k = 1;
 
-N1 = 40;
-N2 = 30;
+% create test data
+[teX1, teX2] = meshgrid(-3.75:0.25:3.75, -3.75:0.25:3.75);
+teY = zeros(size(teX1));
 
-% Create training data
-r1 = mvnrnd([-1.0, -0.5], eye(2), N1);
-r2 = mvnrnd([+1.5, +1.0], 2*eye(2), N2);
-
-hold on;
-xlim([-4, 4]);
-ylim([-4, 4]);
-plot(r1(:,1), r1(:, 2), 'o', 'MarkerSize', 10, 'LineWidth', 1.5, 'color', 'red');
-plot(r2(:,1), r2(:, 2), 'o', 'MarkerSize', 10, 'LineWidth', 1.5, 'color', 'blue');
-
-% Create test data
-[X, Y] = meshgrid(-3.75:0.25:3.75, -3.75:0.25:3.75);
-Z = zeros(size(X));
-
-% Create class column
-r = [[r1, zeros(N1, 1); r2, ones(N2, 1)], zeros(N1 + N2, 1)];
-
-for i = 1 : size(X)
-    for j = 1 : size(Y)
-        % Create distance column
-        r(:, 4) = pdist2(r(:,1:2), [X(i, j), Y(i, j)]);
-        [~, idx] = sort(r(:, 4));
-        classes = r(idx, 3);
-        Z(i, j) = round(mean(classes(1:k)));
+for i = 1 : size(teX1)
+    for j = 1 : size(teX2)
+        % calculate distances
+        dists = pdist2(X, [teX1(i, j), teX2(i, j)]);
+        [~, idx] = sort(dists);
+        classes = y(idx);
+        % assign class on majority vote
+        teY(i, j) = round(1/k * sum(classes(1:k)));
     end
 end
 
-contour(X, Y, Z, [1,1], 'black');
+%% Plot data
+hold on; xlim([-4, 4]); ylim([-4, 4]);
 
-cl1 = find(Z == 0);
-cl2 = find(Z == 1);
+% Plot training data
+plot(X(y==0, 1), X(y==0, 2), 'o', 'MarkerSize', 10, 'LineWidth', 1.5, ...
+    'Color', 'red');
+plot(X(y==1, 1), X(y==1, 2), 'o', 'MarkerSize', 10, 'LineWidth', 1.5, ...
+    'Color', 'blue');
 
-X0 = X(cl1); Y0 = Y(cl1); X1 = X(cl2); Y1 = Y(cl2);
+% Plot decision boundaries
+contour(teX1, teX2, teY, [1,1], 'black');
 
-plot(X0(:), Y0(:), '+', 'MarkerSize', 2, 'LineWidth', 0.1, 'color', 'red');
-plot(X1(:), Y1(:), '+', 'MarkerSize', 2, 'LineWidth', 0.1, 'color', 'blue');
+% Plot predictions
+plot(teX1(teY==0), teX2(teY==0), '+', 'MarkerSize', 2, 'LineWidth', 0.1, ...
+    'Color', 'red');
+plot(teX1(teY==1), teX2(teY==1), '+', 'MarkerSize', 2, 'LineWidth', 0.1, ...
+    'Color', 'blue');
