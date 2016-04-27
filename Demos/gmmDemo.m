53dc4b59d822e3cad3f25722451060c11ace08d2
jcboyd <boyd.joseph.c@gmail.com>
2016-04-27 10:59:00 +0200

    first commit

--- Demos/gmmDemo.m
+++ Demos/gmmDemo.m
@@ -1,65 +1,60 @@
-% Gaussian Mixture Models (GMM) with expectation maximisation on randomly generated data
-% Note pause (line 53) to observe visualisation progress
-%
-
-% Generate data
-
-mu1 = [1 -1];
-mu2 = [2, 1];
-mu3 = [3, 2];
-sigma1 = [.17 .34; .34 .85];
-sigma2 = [.90 .40; .40 .30];
-sigma3 = [.34 -.30; -.30 .36];
-
-N = 180;
-
-X1 = mvnrnd(mu1,sigma1,N/3);
-X2 = mvnrnd(mu2,sigma2,N/3);
-X3 = mvnrnd(mu3,sigma3,N/3);
-
-X = [X1; X2; X3];
-
-grid on;
+% Gaussian Mixture Models (GMM) with expectation maximisation on randomly
+% generated data. Note pause (line 53) to observe visualisation progress.
+clear all;
+load('gmm_data.mat');
+[N, ~] = size(X);
 
 % EM algorithm
-
 K = 3;
 
-I = eye(2);
-Pi =  ones(K, 1) / K; Sigma = repmat(I, K, 1); Mu = rand(K, 2);
+% Initialise parameters
+Mu = cell(K, 1); Sigma = cell(K, 1); Pi = cell(K, 1);
+
+for k = 1 : K
+    Mu{k} = min(X) + (max(X) - min(X)) .* rand(1, 2);
+    Sigma{k} = eye(2);
+    Pi{k} = 1 / K;
+end
+
+% Initialise marginals
 P = zeros(N, K);
 
-numiters = 1; maxiters = 30;
+numiters = 0; maxiters = 10;
 
 while numiters < maxiters
     % E-step
-    for n = 1:N
-        for k = 1:K
-            P(n, k) = Pi(k) * mvnpdf(X(n, :), Mu(k, :), Sigma((2*(k - 1) + 1):2*k, :));
+    for n = 1 : N
+        for k = 1 : K
+            P(n, k) = Pi{k} * mvnpdf(X(n, :), Mu{k}, Sigma{k});
         end
         P(n, :) = P(n, :) ./ sum(P(n, :));
         % Plot observations
-        hold on;
-        plot(X(n, 1), X(n, 2), 'o', 'color', [P(n, 1), P(n, 2), P(n, 3)], 'MarkerSize', 6, 'MarkerFaceColor', [P(n, 1), P(n, 2), P(n, 3)]);
+        hold on; grid on;
+        plot(X(n, 1), X(n, 2), 'o', 'MarkerSize', 8, 'LineWidth', 1, ...
+            'Color', [P(n, 1), P(n, 2), P(n, 3)], ...
+            'MarkerFaceColor', [P(n, 1), P(n, 2), P(n, 3)], ...
+            'MarkerEdgeColor', [0.5 * P(n, 1), 0.5 * P(n, 2), 0.5 * P(n, 3)]);
     end
     hold off;
     pause
     % M-step
-    for k = 1:K
-        Mu(k, :) = sum(diag(P(:, k)) * X, 1) / sum(P(:, k));
-        dev = X - repmat(Mu(k, :), N, 1);
-        Sigma((2*(k - 1) + 1):2*k, :) = transpose(dev) * diag(P(:, k)) * dev / sum(P(:, k));
-        Pi(k) = sum(P(:, k)) / N;
+    for k = 1 : K
+        % Optimise each parameter
+        Mu{k} = sum(diag(P(:, k)) * X, 1) / sum(P(:, k));
+        dev = X - repmat(Mu{k}, N, 1);
+        Sigma{k} = dev' * diag(P(:, k)) * dev / sum(P(:, k));
+        Pi{k} = sum(P(:, k)) / N;
     end
     numiters = numiters + 1;
 end
 
 % Plot contours
 x1 = -5:.05:5; x2 = -5:.05:5;
-[X1,X2] = meshgrid(x1,x2);
+[X1, X2] = meshgrid(x1, x2);
+
 for k = 1:K
     hold on;
-    F = mvnpdf([X1(:) X2(:)], Mu(k, :), Sigma((2*(k - 1) + 1):2*k, :));
-    F = reshape(F,length(x2),length(x1));
-    contour(x1,x2,F,.25,'color', 'black', 'LineWidth', 2);
+    F = mvnpdf([X1(:) X2(:)], Mu{k}, Sigma{k});
+    F = reshape(F, length(x2), length(x1));
+    contour(x1, x2, F, .25, 'color', 'black', 'LineWidth', 1);
 end
